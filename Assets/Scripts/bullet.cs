414237dca3e83fa45aa5e075c4d6760b38e7404e
Jackcool81 <76261988+Jackcool81@users.noreply.github.com>
2023-04-14 21:03:23 -0700

    Update bullet.cs

--- Assets/Scripts/bullet.cs
+++ Assets/Scripts/bullet.cs
@@ -7,7 +7,7 @@ public class Bullet : MonoBehaviour
 	private GameObject player;
 	void Start() {
 		player = GameObject.Find("Player");
-		Destroy(this.gameObject, 3);
+		Destroy(gameObject, 3);
 	}
 	void OnCollisionEnter2D(Collision2D collision)
 	{
@@ -17,7 +17,7 @@ public class Bullet : MonoBehaviour
 			if (collision.gameObject.GetComponent<Walker>().health <= 0) {
 				Destroy(collision.gameObject);
 				player.GetComponent<Player>().coins = player.GetComponent<Player>().coins + 5;
-				Destroy(this.gameObject);
+				Destroy(gameObject);
 			}
 		}  
 	}
