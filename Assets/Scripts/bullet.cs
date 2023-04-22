d91fe43c9b254369ce98b972c120ae452be20fa7
Leonprolol <105960425+Leonprolol@users.noreply.github.com>
2023-04-21 21:32:40 -0700

    yes

--- Assets/Scripts/bullet.cs
+++ Assets/Scripts/bullet.cs
@@ -7,19 +7,21 @@ public class bullet : MonoBehaviour
 	private GameObject player;
 	void Start() {
 		player = GameObject.Find("Player");
-		Destroy(gameObject, 3);
+		
 	}
 	void OnCollisionEnter2D(Collision2D collision)
 	{
 		print("hello");
 		if (collision.gameObject.tag == "Enemy")
-		{						
+		{	
+			print("sussy baka");					
 			collision.gameObject.GetComponent<Walker>().health = collision.gameObject.GetComponent<Walker>().health - 25;
 			if (collision.gameObject.GetComponent<Walker>().health <= 0) {
 				Destroy(collision.gameObject);
 				player.GetComponent<Player>().coins = player.GetComponent<Player>().coins + 5;
-				Destroy(gameObject);
+			
 			}
 		}  
+			Destroy(gameObject);
 	}
 }
--- Assets/attack.cs
+++ Assets/attack.cs
@@ -6,9 +6,12 @@ public class attack : MonoBehaviour
 {
     public GameObject bullet;
     public GameObject firepoint;
+    public GameObject gun;
+    public GameObject knifeReal;
     public int ammo = 100;
     public float cooldown = 1.5f;
     private bool canAttack = true;
+    private bool knife = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,17 +21,36 @@ public class attack : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+         if (Input.GetKeyDown(KeyCode.Alpha1)){
+            knife = true;
+                knifeReal.SetActive(true);
+            gun.SetActive(false);
+         }
+
+        if (Input.GetKeyDown(KeyCode.Alpha2)){
+            knife = false;
+               knifeReal.SetActive(false);
+            gun.SetActive(true);
+         }
      if (Input.GetMouseButtonDown(0)){
-        if (ammo > 0 && canAttack == true) {
+        if (knife == true) {
+        
+        }
+        else {
+         
+            if (ammo > 0 && canAttack == true) {
             GameObject bulletClone = Instantiate(bullet, firepoint.transform.position, Quaternion.identity); 
             bulletClone.GetComponent<Rigidbody2D>().velocity = new Vector2(transform.localScale.x, 0)*100;
             ammo = ammo-1;
             canAttack = false;
             StartCoroutine(attackcooldown());
+            }
         }
+    
        
      }   
-     if (Input.GetKeyDown(KeyCode.R)) {
+     if (Input.GetKeyDown(KeyCode.R) && knife == false) {
         ammo = 100;
      }
     }
