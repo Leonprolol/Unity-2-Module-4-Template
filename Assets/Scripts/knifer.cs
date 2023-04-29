909de43a7af8ddd9d45e0a4b8784f81f7e1932c1
Jack Wagner <76261988+Jackcool81@users.noreply.github.com>
2023-04-28 20:46:55 -0700

    Create knifer.cs

---
+++ Assets/Scripts/knifer.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class knifer : MonoBehaviour
+{
+	private GameObject player;
+	void Start() {
+		player = GameObject.Find("Player");
+		
+	}
+	void OnCollisionEnter2D(Collision2D collision)
+	{
+		print("hello");
+		if (collision.gameObject.tag == "Enemy")
+		{	
+			print("sussy baka");					
+			collision.gameObject.GetComponent<Walker>().health = collision.gameObject.GetComponent<Walker>().health - 25;
+			if (collision.gameObject.GetComponent<Walker>().health <= 0) {
+				Destroy(collision.gameObject);
+				player.GetComponent<Player>().coins = player.GetComponent<Player>().coins + 5;
+			
+			}
+		}  
+			Destroy(gameObject);
+	}
+}
