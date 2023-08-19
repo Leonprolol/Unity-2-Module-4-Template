fe7b7144f7afb7f9a37097354b74f733cf86b89f
Jack Wagner <76261988+Jackcool81@users.noreply.github.com>
2023-08-18 20:50:59 -0700

    Update BulletScript.cs

--- Assets/Scripts/BulletScript.cs
+++ Assets/Scripts/BulletScript.cs
@@ -4,15 +4,18 @@ public class BulletScript : MonoBehaviour
 {
     public float speed = 10f;
     public float lifetime = 2f;
+    private Rigidbody2D rb;
     
     private void Start()
     {
+        rb = GetComponent<Rigidbody2D>();
         Destroy(gameObject, lifetime);
     }
 
     private void Update()
     {
         // Move the bullet forward
+        rb.AddForce(transform.forward * 8000);
         transform.Translate(Vector3.forward * speed * Time.deltaTime);
     }
 }
