eebb21e6275c61b8fe1afcae77ba27232d6d3f5d
Jack Wagner <76261988+Jackcool81@users.noreply.github.com>
2023-09-01 21:14:25 -0700

    Update HomingProjectile.cs

--- Assets/Scripts/HomingProjectile.cs
+++ Assets/Scripts/HomingProjectile.cs
@@ -4,35 +4,38 @@ public class HomingProjectile : MonoBehaviour
 {
     public Transform target; // The target to home in on (e.g., the player)
     public float homingSpeed = 5f; // The speed at which the projectile homes in on the target
-    public float rotationSpeed = 5f; // The speed at which the projectile rotates to face the target
+    public float rotationSpeed = 200f; // The speed at which the projectile rotates to face the target
 
     private void Update()
     {
         if (target == null)
         {
             // If the target is lost or destroyed, just move forward
-            transform.Translate(Vector3.forward * homingSpeed * Time.deltaTime);
+            transform.Translate(Vector2.up * homingSpeed * Time.deltaTime);
             return;
         }
 
         // Calculate the direction to the target
-        Vector3 directionToTarget = (target.position - transform.position).normalized;
+        Vector2 directionToTarget = (target.position - transform.position).normalized;
+
+        // Calculate the angle to rotate towards the target
+        float angle = Mathf.Atan2(directionToTarget.y, directionToTarget.x) * Mathf.Rad2Deg;
 
         // Rotate the projectile towards the target
-        Quaternion targetRotation = Quaternion.LookRotation(directionToTarget);
-        transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+        Quaternion targetRotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
 
         // Move the projectile forward in the direction of the target
-        transform.Translate(Vector3.forward * homingSpeed * Time.deltaTime);
+        transform.Translate(Vector2.up * homingSpeed * Time.deltaTime);
     }
 
-    private void OnTriggerEnter(Collider other)
+    private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject == target.gameObject)
         {
             // Handle what happens when the projectile hits the target (e.g., deal damage)
             // You can add your custom logic here.
-            
+
             // Destroy the projectile upon hitting the target
             Destroy(gameObject);
         }
